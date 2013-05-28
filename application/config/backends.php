7fe0bf14f967f7c193c4cbb87e49e3954630cf74
zaowei20 <zaowei20@gmail.com>
2013-05-28 11:47:33 +0800

    删除web端代码

--- application/config/backends.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-
-// 服务器及端口设置
-$config['backend']['server_ip'] = '127.0.0.1';
-$config['backend']['server_port'] = 13123;
-
-//日志路径设置
-$config['backend']['logpath'] = '/var/www/php-backend/data/log/';
